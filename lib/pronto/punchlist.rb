1c8dbd60e2c96caa8ce4e8f40e3d3c0ab501d3e8
Vince Broz <vince.broz@bluelabs.com>
2019-12-07 16:48:29 -0500

    Initial version from bundle gem

---
+++ lib/pronto/punchlist.rb
@@ -0,0 +1,8 @@
+require "pronto/punchlist/version"
+
+module Pronto
+  module Punchlist
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ lib/pronto/punchlist/version.rb
@@ -0,0 +1,5 @@
+module Pronto
+  module Punchlist
+    VERSION = "0.1.0"
+  end
+end
