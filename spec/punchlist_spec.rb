6ec05103300fd047299e8e783123656daf2f1c13
Vince Broz <vince@broz.cc>
2019-12-15 15:42:05 -0500

    Break out valid_patch? tests

--- spec/punchlist_spec.rb
+++ spec/punchlist_spec.rb
@@ -19,11 +19,6 @@ describe Pronto::Punchlist do
   let(:punchlist) { double('punchlist') }
   let(:patch) { double('patch') }
   let(:filename) { double('filename') }
-  before :each do
-    allow(patch).to receive(:new_file_full_path) do
-      filename
-    end
-  end
 
   describe '#new' do
     subject { pronto_punchlist }
@@ -44,53 +39,6 @@ describe Pronto::Punchlist do
     end
 
     xit 'coordinates with other methods'
-  end
-
-  describe '#valid_patch?' do
-    subject { pronto_punchlist.valid_patch?(patch) }
-
-    context 'with an empty patch' do
-      before :each do
-        expect(patch).to receive(:additions) { 0 }
-      end
-      it 'rejects' do
-        should be false
-      end
-    end
-
-    context 'with a valid file' do
-      before :each do
-        expect(patch).to receive(:additions) { 1 }
-        expect(source_file_globber).to receive(:is_non_binary?)
-          .with(filename) { !file_is_binary }
-      end
-
-      context 'in the Ruby language' do
-        let(:file_is_binary) { false }
-
-        it 'accepts' do
-          should be true
-        end
-      end
-
-      context 'which is binary' do
-        let(:file_is_binary) { true }
-
-        before :each do
-          # TODO - do something like this: https://github.com/apiology/quality/blob/master/lib/quality/linguist_source_file_globber.rb - should I export to its own repo?  Maybe import for now and just test that this calls into that interface?
-          allow(patch).to receive(:patch) { '/foo/bar/baz.bin' }
-        end
-
-        it 'rejects' do
-          should be false
-        end
-      end
-
-      context 'which is a markdown file' do
-        # TODO: Get xit added to punchlist list
-        xit 'accepts'
-      end
-    end
   end
 
   describe '#inspect' do
