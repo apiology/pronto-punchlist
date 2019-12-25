18b33dd12da1610949e342d02e9b53ca194de404
Vince Broz <vince@broz.cc>
2019-12-24 21:16:48 -0500

    RuboCop fixes

--- spec/pronto/punchlist/patch_inspector_multiple_offenses_spec.rb
+++ spec/pronto/punchlist/patch_inspector_multiple_offenses_spec.rb
@@ -30,73 +30,59 @@ describe Pronto::Punchlist::PatchInspector do
       allow(punchlist_driver).to receive(:inspect_filename).with(filename) do
         offenses
       end
+      allow(offense_matcher_class).to receive(:new).with(offense_1) do
+        offense_matcher_1
+      end
+      allow(offense_matcher_class).to receive(:new).with(offense_2) do
+        offense_matcher_2
+      end
+      allow(offense_matcher_1).to receive(:inspect_patch).with(patch) do
+        offense_1_results
+      end
+      allow(offense_matcher_2).to receive(:inspect_patch).with(patch) do
+        offense_2_results
+      end
     end
 
     let(:patch) { instance_double(Pronto::Git::Patch, 'patch') }
+    let(:offense_1) { instance_double(Punchlist::Offense, 'offense_1') }
+    let(:offense_2) { instance_double(Punchlist::Offense, 'offense_2') }
+    let(:message_1) { instance_double(Pronto::Message, 'message_1') }
+    let(:message_2) { instance_double(Pronto::Message, 'message_2') }
+    let(:offense_matcher_1) do
+      instance_double(Pronto::Punchlist::OffenseMatcher, 'offense_matcher_1')
+    end
+    let(:offense_matcher_2) do
+      instance_double(Pronto::Punchlist::OffenseMatcher, 'offense_matcher_2')
+    end
+    let(:offenses) { [offense_1, offense_2] }
 
-    context 'when two offenses in file' do
-      let(:offense_1) { double('offense_1') }
-      let(:offense_2) { double('offense_2') }
-      let(:message_1) { instance_double(Pronto::Message, 'message_1') }
-      let(:message_2) { instance_double(Pronto::Message, 'message_2') }
-      let(:offense_matcher_1) do
-        instance_double(Pronto::Punchlist::OffenseMatcher, 'offense_matcher_1')
-      end
-      let(:offense_matcher_2) do
-        instance_double(Pronto::Punchlist::OffenseMatcher, 'offense_matcher_2')
-      end
-      let(:offenses) { [offense_1, offense_2] }
+    context 'when two_files related to patch' do
+      let(:offense_1_results) { message_1 }
+      let(:offense_2_results) { message_2 }
 
-      before do
-        expect(offense_matcher_class).to receive(:new).with(offense_1) do
-          offense_matcher_1
-        end
-        expect(offense_matcher_class).to receive(:new).with(offense_2) do
-          offense_matcher_2
-        end
-        expect(offense_matcher_1).to receive(:inspect_patch).with(patch) do
-          offense_1_results
-        end
-        expect(offense_matcher_2).to receive(:inspect_patch).with(patch) do
-          offense_2_results
-        end
-      end
+      it { is_expected.to eq [message_1, message_2] }
+    end
 
-      context 'when both related to patch' do
-        let(:offense_1_results) { message_1 }
-        let(:offense_2_results) { message_2 }
+    context 'when only first file related to patch' do
+      let(:offense_1_results) { message_1 }
+      let(:offense_2_results) { nil }
 
-        it 'returns both offenses' do
-          should eq [message_1, message_2]
-        end
-      end
+      it { is_expected.to eq [message_1] }
+    end
 
-      context 'when only first related to patch' do
-        let(:offense_1_results) { message_1 }
-        let(:offense_2_results) { nil }
+    context 'when only second file related to patch' do
+      let(:offense_1_results) { nil }
+      let(:offense_2_results) { message_2 }
 
-        it 'returns only first' do
-          should eq [message_1]
-        end
-      end
+      it { is_expected.to eq [message_2] }
+    end
 
-      context 'when only second related to patch' do
-        let(:offense_1_results) { nil }
-        let(:offense_2_results) { message_2 }
+    context 'when both files unrelated to patch' do
+      let(:offense_1_results) { nil }
+      let(:offense_2_results) { nil }
 
-        it 'returns only second' do
-          should eq [message_2]
-        end
-      end
-
-      context 'when both unrelated to patch' do
-        let(:offense_1_results) { nil }
-        let(:offense_2_results) { nil }
-
-        it 'returns nothing' do
-          should eq []
-        end
-      end
+      it { is_expected.to eq [] }
     end
   end
 end
--- spec/pronto/punchlist/patch_inspector_spec.rb
+++ spec/pronto/punchlist/patch_inspector_spec.rb
@@ -5,10 +5,11 @@ require_relative '../../spec_helper'
 require 'pronto/punchlist/patch_inspector'
 
 describe Pronto::Punchlist::PatchInspector do
+  subject { inspector.inspect_patch(patch) }
+
   let(:inspector) do
-    Pronto::Punchlist::PatchInspector
-      .new(punchlist_driver: punchlist_driver,
-           offense_matcher_class: offense_matcher_class)
+    described_class.new(punchlist_driver: punchlist_driver,
+                        offense_matcher_class: offense_matcher_class)
   end
   let(:offense_matcher_class) do
     class_double(Pronto::Punchlist::OffenseMatcher)
@@ -17,63 +18,51 @@ describe Pronto::Punchlist::PatchInspector do
     instance_double(Pronto::Punchlist::PunchlistDriver,
                     'punchlist_driver')
   end
-  let(:patch) { double('patch') }
-  let(:filename) { double('filename') }
+  let(:patch) { instance_double(Pronto::Git::Patch) }
+  let(:filename) { instance_double(String, 'filename') }
+
   before do
     allow(patch).to receive(:new_file_full_path) do
       filename
     end
+    allow(punchlist_driver).to receive(:inspect_filename).with(filename) do
+      offenses
+    end
   end
 
-  describe '#inspect' do
-    subject { inspector.inspect_patch(patch) }
+  context 'when no offenses are in file' do
+    let(:offenses) { [] }
+
+    it { is_expected.to eq [] }
+  end
+
+  context 'when one offense in file' do
+    let(:offense) { instance_double(Punchlist::Offense, 'offense') }
+    let(:offenses) { [offense] }
+    let(:offense_matcher) do
+      instance_double(Pronto::Punchlist::OffenseMatcher)
+    end
 
     before do
-      expect(punchlist_driver).to receive(:inspect_filename).with(filename) do
-        offenses
+      allow(offense_matcher_class).to receive(:new).with(offense) do
+        offense_matcher
+      end
+      allow(offense_matcher).to receive(:inspect_patch).with(patch) do
+        message
       end
     end
 
-    let(:patch) { instance_double(Pronto::Git::Patch) }
+    context 'when related to patch' do
+      let(:message) { instance_double(Pronto::Message) }
 
-    context 'when no offenses are in file' do
-      let(:offenses) { [] }
-      it 'returns nothing' do
-        should eq []
-      end
+      it { is_expected.to eq [message] }
     end
 
-    context 'when one offense in file' do
-      let(:offense) { double('offense') }
-      let(:offenses) { [offense] }
-      let(:offense_matcher) do
-        instance_double(Pronto::Punchlist::OffenseMatcher)
-      end
-      before do
-        expect(offense_matcher_class).to receive(:new).with(offense) do
-          offense_matcher
-        end
-        expect(offense_matcher).to receive(:inspect_patch).with(patch) do
-          message
-        end
-      end
+    context 'when unrelated to patch' do
+      let(:offense_line) { after_end_of_change_line }
+      let(:message) { nil }
 
-      context 'and related to patch' do
-        let(:message) { instance_double(Pronto::Message) }
-
-        it 'returns message' do
-          should eq [message]
-        end
-      end
-
-      context 'when unrelated to patch' do
-        let(:offense_line) { after_end_of_change_line }
-        let(:message) { nil }
-
-        it 'returns nothing' do
-          should eq []
-        end
-      end
+      it { is_expected.to eq [] }
     end
   end
 end
