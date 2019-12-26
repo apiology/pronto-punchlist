a8c4321a73d332833c8a538ff635762864d61373
Vince Broz <vince@broz.cc>
2019-12-26 16:29:07 -0500

    Finish file classifier specs

--- lib/pronto/punchlist/file_classifier.rb
+++ lib/pronto/punchlist/file_classifier.rb
@@ -2,13 +2,15 @@ module Pronto
   class Punchlist < Runner
     class FileClassifier
       def initialize(source_file_globber: SourceFinder::SourceFileGlobber.new)
+        @source_file_globber = source_file_globber
       end
 
-      def is_non_binary?(filename)
-        # parse extension out
-        # check extension against list in source_file_globber
+      def is_non_binary?(path)
         # https://ruby-doc.org/core-2.5.1/File.html#method-c-fnmatch
-        true
+        # EXTGLOB enables ',' as part of glob language
+        File.fnmatch?(@source_file_globber.source_and_doc_files_glob,
+                      path,
+                      File::FNM_EXTGLOB)
       end
     end
   end
--- spec/pronto/punchlist/file_classifier_spec.rb
+++ spec/pronto/punchlist/file_classifier_spec.rb
@@ -11,39 +11,45 @@ describe Pronto::Punchlist::FileClassifier do
     subject { file_classifier.is_non_binary?(filename) }
 
     context 'when Ruby' do
-      let(:filename) { instance_double(String, 'foo.rb') }
+      let(:filename) { 'foo.rb' }
 
       it { is_expected.to be(true) }
     end
 
     context 'when Unix executable' do
-      let(:filename) { instance_double(String, 'ls') }
+      let(:filename) { 'ls' }
 
-      xit { is_expected.to be(false) }
+      it { is_expected.to be(false) }
+    end
+
+    context 'when differnet Ruby file' do
+      let(:filename) { 'foo.rb' }
+
+      it { is_expected.to be(true) }
     end
 
     context 'when DOS executable' do
-      let(:filename) { instance_double(String, 'dir.exe') }
+      let(:filename) { 'dir.exe' }
 
-      xit { is_expected.to be(false) }
+      it { is_expected.to be(false) }
     end
 
-    context 'when Gemfile' do
-      let(:filename) { instance_double(String, 'Gemfile') }
+    context 'when Rakefile' do
+      let(:filename) { 'Rakefile' }
 
-      xit { is_expected.to be(true) }
+      it { is_expected.to be(true) }
     end
 
     context 'when Python' do
-      let(:filename) { instance_double(String, 'bar.py') }
+      let(:filename) { 'bar.py' }
 
-      xit { is_expected.to be(true) }
+      it { is_expected.to be(true) }
     end
 
     context 'when Markdown' do
-      let(:filename) { instance_double(String, 'bar.md') }
+      let(:filename) { 'bar.md' }
 
-      xit { is_expected.to be(true) }
+      it { is_expected.to be(true) }
     end
   end
 end
